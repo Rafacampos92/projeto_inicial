e9124c80600d09b4ea239de61b390e98f064f868
Rafael Almeida <rafael_almeidacampos@hotmail.com>
2024-05-26 11:24:11 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,19 @@
+print ("jornada de dados")
+
+# Variáveis
+idade = 32
+nome = "Rafael"
+
+# Print
+print (idade)
+
+# Soma
+nova_idade = idade +1
+print (nova_idade)
+
+# Lista
+lista = [1,2,3,4,5]
+
+# Métodos
+lista_invertida = lista.reverse()
+lista_invertida
